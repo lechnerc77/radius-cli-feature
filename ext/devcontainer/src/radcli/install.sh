129765e7e5165695991a5e8d8604d11425e2309f
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-12-03 15:12:19 +0100

    adjustments

--- ext/devcontainer/src/radcli/install.sh
+++ ext/devcontainer/src/radcli/install.sh
@@ -6,6 +6,20 @@ CLI_VERSION="${VERSION:-"latest"}"
 # Ensure apt is in non-interactive to avoid prompts
 export DEBIAN_FRONTEND=noninteractive
 
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt-get update..."
+            apt-get update -y
+        fi
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+echo "(*) Ensuring dependencies are installed"
+
+check_packages wget
+
 echo "(*) Installing Radius CLI"
 
 # If version is not specified, install latest otherwise install edge
