d8b2965e6ef99be2b8687189d7c9fcfe3612a519
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-12-08 15:54:00 +0100

    test

--- ext/devcontainer/src/radcli/install.sh
+++ ext/devcontainer/src/radcli/install.sh
@@ -23,12 +23,9 @@ check_packages wget
 echo "(*) Installing Radius CLI"
 
 # If version is not specified, install latest otherwise install edge
-if [ "${CLI_VERSION}" == "latest"]; then
+if [ "${CLI_VERSION}" = "latest" ]; then
     wget -q "https://raw.githubusercontent.com/radius-project/radius/main/deploy/install.sh" -O - | /bin/bash
-elif [ "${CLI_VERSION}" = "edge" ]; then
+else
     wget -O /usr/local/bin/rad https://get.radapp.dev/tools/rad/edge/linux-x64/rad 
     chmod +rx /usr/local/bin/rad
-else
-  wget -q "https://raw.githubusercontent.com/radius-project/radius/main/deploy/install.sh" -O - | /bin/bash -s "$CLI_VERSION"    
 fi
-    
